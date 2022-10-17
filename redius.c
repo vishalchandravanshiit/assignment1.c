79ce268a6fc36a5a4277925222c0d77153e505b1
vishalchandravanshiit <112495479+vishalchandravanshiit@users.noreply.github.com>
2022-10-17 13:11:25 +0530

    Add files via upload

---
+++ redius.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int
+main ()
+{
+  float radius, Area;
+             printf ("enter the redius of circule\n ");
+                scanf ("%f", &radius);
+                    Area = 3.14 * radius * radius;
+	printf ("the Radius of circul is = %f \n and the area is %f", radius, Area);
+                        
+                        
+  return 0;
+}
+
